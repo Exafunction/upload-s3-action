4babb96a6ad9b44f599e0d38aaf933b33eda1458
Nathan Juan <nathan.juan@codeium.com>
2024-09-12 21:51:06 +0000

    patch dist

--- dist/index.js
+++ dist/index.js
@@ -28943,7 +28943,7 @@ function run() {
         ACL: 'public-read',
         Body: fileStream,
         Key: bucketPath,
-        ContentType: lookup(p.path) || 'text/plain',
+        ContentType: lookup(p.path) || 'application/octet-stream',
       };
       return upload(params);
     })
